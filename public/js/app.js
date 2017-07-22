82f5f13aba0715848865c9d2487ee57629f0a6b3
Rachel Mills <rachel.h.mills@gmail.com>
2017-07-22 12:59:50 +1000

    Add support for chat rooms.

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,33 @@
 var socket = io();
 var name = getQueryVariable('name') || 'Anonymous';
-var room = getQueryVariable('room');
+var room = getQueryVariable('room') || 'Not in a room';
 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
+
+jQuery('.room-title').text(room);
 
 socket.on('message', function(message) {
 	var timeStampMoment = moment.utc(message.timeStamp);
 	var $message = jQuery('.messages');
-	
+
 	console.log('New message:');
 	console.log(message.text);
 
-	$message.append('<p><strong>' + message.name + ' ' + timeStampMoment.local().format('h:mm a') +'</strong></p>');
+	$message.append('<p><strong>' + message.name + ' ' + timeStampMoment.local().format('h:mm a') + '</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
 });
 
-var $form = jQuery('#message-form'); // use # to select by id   the $ is to say that the variable stores a jQuery instance of an element (has access to all jQuery methods)
+var $form = jQuery('#message-form'); // use # to select by id   the $ is to say that the variable stores a 
+									// jQuery instance of an element (has access to all jQuery methods)
 
 $form.on('submit', function(event) {
-	event.preventDefault();   // this prevents entire page refreshing on submit
+	event.preventDefault(); // this prevents entire page refreshing on submit
 
 	var $message = $form.find('input[name=message]');
 
--- server.js
+++ server.js
@@ -8,15 +8,27 @@ var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function(socket) { // listen for events
 	console.log('User connected via socket.io');
+
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined',
+			timeStamp: moment().valueOf()
+		});
+	});
 
 	socket.on('message', function(message) {
 		var now = moment();
 		message.timeStamp = now.valueOf();
 		console.log('Message received:  ' + message.text);
 		// socket.broadcast.emit('message', message);  // this broadcasts to everyone except you
-		io.emit('message', message);	// this broadcasts to everyone
+		io.to(clientInfo[socket.id].room).emit('message', message);	// this broadcasts to everyone
 	});
 
 	socket.emit('message', {
