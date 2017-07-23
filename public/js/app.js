0dbcf4b9e33876127771c1fe2fd64deb4666d0c7
Rachel Mills <rachel.h.mills@gmail.com>
2017-07-23 12:17:25 +1000

    Add styles for chat app.

--- public/js/app.js
+++ public/js/app.js
@@ -14,13 +14,14 @@ jQuery('.room-title').text(room);
 
 socket.on('message', function(message) {
 	var timeStampMoment = moment.utc(message.timeStamp);
-	var $message = jQuery('.messages');
-
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>')
 	console.log('New message:');
 	console.log(message.text);
 
 	$message.append('<p><strong>' + message.name + ' ' + timeStampMoment.local().format('h:mm a') + '</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
 });
 
 var $form = jQuery('#message-form'); // use # to select by id   the $ is to say that the variable stores a 
